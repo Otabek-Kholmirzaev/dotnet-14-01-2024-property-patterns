c73d3a049b1de3c4d9dfa7de92a5a2823f173b84
Otabek Kholmirzaev <otabekmirzo@outlook.com>
2024-01-14 01:26:20 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿var emp = new Employee(49, "Otabek", 1000);
+Console.WriteLine(emp.Salary);
+emp.GiveBonus(100);
+Console.WriteLine(emp.Salary);
+
+
+class Employee
+{
+    int _age;
+    string _name;
+    int _salary; 
+    public int Salary 
+    {
+        get => _salary;
+        set => _salary = value;
+    }
+    public Employee(int age, string name, int salary)
+    {
+        _age = age;
+        _name = name;
+        _salary = salary;
+    }
+    
+    public void GiveBonus(int amount)
+    {
+        _salary = this switch 
+        {
+            {_age: > 50 } => _salary + amount,
+            _ => _salary + amount / 2
+        };
+    }
+}
